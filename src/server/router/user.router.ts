43d98f0710d213b0e03a42148f4434bcb8e7bba3
jooyeal <jyol1234@gmail.com>
2022-10-07 15:15:08 +0900

    build error

--- src/server/router/user.router.ts
+++ src/server/router/user.router.ts
@@ -1,7 +1,7 @@
 import { t } from "../trpc";
 import * as trpc from "@trpc/server";
 import { loginUserSchema, registUserSchema } from "../../schema/user.schema";
-import prisma from "../../utils/prisma";
+import { prisma } from "../../utils/prisma";
 
 export const userRouter = t.router({
   regist: t.procedure.input(registUserSchema).mutation(async ({ input }) => {
