e2bb6f45033a4c7c20d60f06b951a87d0e0ef57e
jooyeal <jyol1234@gmail.com>
2022-10-08 13:31:13 +0900

    setting docker

--- src/server/router/user.router.ts
+++ src/server/router/user.router.ts
@@ -1,22 +1,24 @@
 import { t } from "../trpc";
 import * as trpc from "@trpc/server";
-import { loginUserSchema, registUserSchema } from "../../schema/user.schema";
+import { registUserSchema } from "../../schema/user.schema";
 import { prisma } from "../../utils/prisma";
+import { Prisma } from "@prisma/client";
 
 export const userRouter = t.router({
   regist: t.procedure.input(registUserSchema).mutation(async ({ input }) => {
-    const isEmailExisted = await prisma.user.findFirst({
-      where: {
-        email: input.email,
-      },
-    });
-    if (isEmailExisted) {
-      throw new trpc.TRPCError({
-        code: "CONFLICT",
-        message: "This email is already existed",
-      });
-    } else {
+    try {
       await prisma.user.create({ data: { ...input } });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        switch (e.code) {
+          case "P2002":
+            throw new trpc.TRPCError({
+              code: "CONFLICT",
+              message: "This email is already existed",
+            });
+        }
+      }
+      throw e;
     }
   }),
 });
